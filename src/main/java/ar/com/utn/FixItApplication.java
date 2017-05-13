2cce558b9aa2aedb86085930fc6a0c5bc009a158
Julian Salischiker <jusalis@gmail.com>
2017-05-13 14:50:32 -0300

    Initial commit

---
+++ src/main/java/ar/com/utn/FixItApplication.java
@@ -0,0 +1,12 @@
+package ar.com.utn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixItApplication.class, args);
+	}
+}
