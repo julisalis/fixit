afa56c35b578c7484a5a8f0a207c29272bf3f0d5
Iara <iedelstein@candoit.com.ar>
2017-08-05 21:04:15 -0300

    modif tel

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,3 +4,12 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
