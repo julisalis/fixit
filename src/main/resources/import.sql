824b2f42780560c69a16c9813acd78cb4681e193
Iara <edeliaru@gmail.com>
2017-08-10 21:12:27 -0300

    provincias y localidades para la base

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
-insert into usuarios (id, email, apellido, nombre, password, username) values (1, "jusalis@gmail.com", "Salischiker", "Julian", "password", "julisalis");
-insert into roles values (1, "ADMIN");
-insert into usuarios_roles values (1,1);
+-- insert into usuarios (id, email, apellido, nombre, password, username) values (1, "jusalis@gmail.com", "Salischiker", "Julian", "password", "julisalis");
+-- insert into roles values (1, "ADMIN");
+-- insert into usuarios_roles values (1,1);
+
+-- INSERT INTO provincia (nombre) VALUES('Buenos Aires'),('Capital Federal'),('Catamarca'),('Chaco'),('Chubut'),('Córdoba'),('Corrientes'),('Entre Rios'),('Formosa'),('Jujuy'),('La Pampa'),('La Rioja'),('Mendoza'),('Misiones'),('Neuquén'),('Rio Negro'),('Salta'),('San Juan'),('San Luis'),('Santa Cruz'),('Santa Fe'),('Santiago del Estero'),('Tierra del Fuego'),('Tucuman');
+
