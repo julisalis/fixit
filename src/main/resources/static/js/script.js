aa01834f77b0c7cc42a5fe8e12c9b8e12c6bde01
Iara <iedelstein@candoit.com.ar>
2017-08-31 12:14:46 -0300

    fix js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,12 +13,12 @@ $(function () {
         $(this).addClass("selected");
         var img = $(this).find(".img-selected");
         var name = img.attr("name");
-        img.prop("src",'images/'+name+"-selected.png");
+        img.prop("src",'/images/'+name+"-selected.png");
     }, function () {
         $(this).removeClass("selected");
         var img = $(this).find(".img-selected");
         var name = img.attr("name");
-        img.prop("src",'images/'+name+".png");
+        img.prop("src",'/images/'+name+".png");
     });
 
     $.validate({
