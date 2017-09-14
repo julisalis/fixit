0b8fa7c94ceeaabc1bf3bccc145aadbffeed45de
Iara <edeliaru@gmail.com>
2017-09-14 01:36:42 -0300

    cambios imagen

--- src/main/resources/static/js/publicacion.js
+++ src/main/resources/static/js/publicacion.js
@@ -48,10 +48,21 @@ $(function () {
         });
 
     $("a#add-photo-link input").on('change', function () {
-        readFile(this);
+        if($('.image_thumbnail').length <= 8){
+            readFile(this);
+        }else {
+            //show error
+            $("a#add-photo-link input").prepend('<input class="error"> Ha alcanzado el límite de fotos </input>')
+        }
+
 
     });
 
+    $("#preview_images .image_thumbnail.view").hover(function () {
+        $(this).find($("buttons")).show();
+    }, function () {
+        $(this).find($("buttons")).hide();
+    });
 });
 
 var count=-1;
@@ -60,26 +71,21 @@ function firstAvailableDate() {
     return date;
 }
 
-
-
-
-
 function readFile(input) {
-    debugger;
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
+            $(".photo-section  #preview_images").show();
             count=count+1;
             var img_div = $('#image_template .image_thumbnail').clone();
             var images_container = $("#preview_images");
             images_container.show();
             images_container.append(img_div);
-            var input = $('<input class="added_'+count+'" type="hidden">').attr('value', e.target.result).attr("name", "files["+count+"].cover");
+            var img = $("img", img_div).attr('src', e.target.result);
+            var input = $('<input class="added_'+count+'" type="hidden">').attr('value', e.target.result).attr("name", "files["+count+"]");
             img_div.append(input);
+
         }
-
-
-
         reader.readAsDataURL(input.files[0]);
 
     } else {
@@ -88,22 +94,16 @@ function readFile(input) {
 }
 
 //Eliminado de foto
-// $('a.remove').click(function(e) {
-//     removeImage($(this),e);
-// });
+$('a.remove').click(function(e) {
+    removeImage($(this),e);
+});
 
-// function removeImage(current, event){
-//     event.preventDefault();
-//     var identifier_group = current.data('identifier-group');
-//     if (typeof identifier_group !== typeof undefined && identifier_group !== false) {
-//         //Guardo en una lista para eliminacion
-//         var input = $('<input type="hidden">').attr('value', identifier_group).attr("name", "removed");
-//         current.closest('form').append(input);
-//     }else{
-//         current.closest('.view').remove();
-//     }
-//     current.closest('.view').hide();
-// }
+function removeImage(current, event){
+    event.preventDefault();
+    var input = $('<input type="hidden">').attr('value', current.data('id-image')).attr("name", "removed");
+    current.closest('form').append(input);
+    current.closest('.view').hide();
+}
 
 
 
