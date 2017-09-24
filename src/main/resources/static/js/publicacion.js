c2548b468ac137898f526c6c395cb29b1681a4d0
mjarabroviski <melaniejarabroviski@hotmail.com>
2017-09-24 17:05:32 -0300

    las publicaciones muestran el detalle cuando las tocas

--- src/main/resources/static/js/publicacion.js
+++ src/main/resources/static/js/publicacion.js
@@ -49,7 +49,7 @@ $(function () {
         minDate:firstAvailableDate(),
         });
 
-    $("#modal-publicacion").on("click",  function () {
+    $(".modal-publicacion").on("click",  function () {
         var publicacionId = $(this).data('id');
 
         $.get('/publicacion/detalle', {publicacionId: publicacionId} )
@@ -57,6 +57,11 @@ $(function () {
                 if (typeof(data.publicacion) != 'undefined'){
                     $('#publicacion').modal('toggle');
                     $("#publicacion .modal-title").text( data.publicacion.titulo);
+                    $("#publicacion .modal-descripcion").text( data.publicacion.descripcion);
+                    $("#publicacion .modal-ubicacion").text( data.publicacion.localidad.nombre+', '+data.publicacion.provincia.nombre);
+                    $("#publicacion .modal-precio").text('Hasta '+ data.publicacion.presupMax+' '+data.publicacion.currency);
+                    $("#publicacion .modal-urgencia").text( data.publicacion.urgencia);
+                    $("#publicacion .modal-fecha").text(data.publicacion.fecha.dayOfMonth+'/'+data.publicacion.fecha.monthValue+'/'+data.publicacion.fecha.year);
                 }
 
 
